5de04499beb413ba946c92d3832c0778ec13a151
aayush-niroula <137910895+aayush-niroula@users.noreply.github.com>
2024-12-19 16:12:59 +0545

    Add files via upload

---
+++ sript.js
@@ -0,0 +1,19 @@
+function searchFunction() {
+  const query = document.getElementById('search').value.trim();
+  if (query) {
+    alert(`You searched for: ${query}`);
+  } else {
+    alert('Please enter a search term.');
+  }
+}
+
+
+
+// function searchFunction() {
+//   const query = document.getElementById('search').value.trim();
+//   if (query) {
+//     alert(`You searched for: ${query}`);
+//   } else {
+//     alert('Please enter a search term.');
+//   }
+// }
